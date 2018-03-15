eefd746a942954f710b478ad75ef654cd742c743
eugene <eugeneyaroshenko@gmail.com>
2018-03-15 18:11:46 +0200

    after review fixes

--- src/views/Profile/account.vue
+++ src/views/Profile/account.vue
@@ -5,14 +5,14 @@
     </div>
     <h2 class="profile-account__header">Welcome to your account, {{userData.given_name}}</h2>
     <div class="account-options">
-      <div class="account-options__option grey">
-        <h2 class="account-options-__heading">Past Transactions</h2>
+      <div class="account-options__option transactions">
+        <h2 class="account-options__heading">Past Transactions</h2>
       </div>
-      <div class="account-options__option">
-        <h2 class="account-options-__heading">Bank Profile</h2>
+      <div class="account-options__option profile">
+        <h2 class="account-options__heading">Bank Profile</h2>
       </div>
-      <div class="account-options__option orange">
-        <h2 class="account-options-__heading">Credit Cards</h2>
+      <div class="account-options__option cards">
+        <h2 class="account-options__heading">Credit Cards</h2>
       </div>
     </div>
   </div>
