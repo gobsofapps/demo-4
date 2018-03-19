558de84d6b2d4e573739e31d5b37cb5297d8ea28
eugene <eugeneyaroshenko@gmail.com>
2018-03-19 16:54:18 +0200

    build with fixes

--- src/views/Profile/account.vue
+++ src/views/Profile/account.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="profile-info" v-if="userData">
+    <div class="profile-logo"></div>
     <div class="welcome-account">Welcome to your account, {{userData.given_name}}</div>
     <div class="control-panel">
       <div class="control-panel__item">Your Savings &gt;</div>
